55f28016d51626df6c8898fd07fd7ee9bd3117d3
Shrinivas <shrinivas4927@gmail.com>
2025-11-28 23:09:52 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,10 +2,10 @@ import sys
 
 if len(sys.argv) == 2:
     script_name = sys.argv[0]
-    string = sys.argv[1]
+    text = sys.argv[1]
 else:
     script_name = sys.argv[0]
-    string = "madam"
+    text = "Madam"
 
 cleaned_text = ''.join(char.lower() for char in text if char.isalnum())
 
